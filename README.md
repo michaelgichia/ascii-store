6c6ae7510e797366f0ba0dc1dbd9ca8f40857cca
michaelgichia <gichuru.gichia@gmail.com>
2018-01-09 12:43:41 +0300

    :sparkles: Added Infinite scrolling, threshhold to load more products.

--- README.md
+++ README.md
@@ -19,51 +19,13 @@ Features
 - to improve the user's experience, we should always pre-emptively fetch the next batch of results in advance, making use of idle-time.  But they still should not be displayed until the user has scrolled to the bottom of the product grid.
 - when the user reaches the end and there are no more products to display, show the message "~ end of catalogue ~".
 
-### Ads features
+### How to run App locally
 
-- after every 20 products we need to insert an advertisement from one of our sponsors. Use the same markup as the advertisement in the header shown in `public/index/html`, but make sure the `?r` query param is randomly generated each time an ad is displayed.
-- Ads should be randomly selected, but a user must never see the same ad twice in a row.
-
-
-Products API
-----
-
-- The basic query looks like this: `/api/products`
-- The response format is JSON.
-- To paginate results use the `_page` parameter, eg: `/api/products?_page=10&_limit=15` (returns 15 results starting from the 10th page).
-- To sort results use the `_sort` parameter, eg: `/api/products?_sort=price`. Valid sort values are `price`, `size` and `id`.
-
-FAQ
-----
-
-### How do I start the app?
-
-Start with `npm start`. The server will look for any files you add to the `public/` directory.
-
-### What libraries/frameworks, packages, tools can I use?
-
-You need to use React.js as the main js library and Git for version control, but other than that you are free to work with any package of your choice.
-
-### What about sort order (ascending / descending)?
-
-We don't need to worry about alternate sort order for this project, we'll just use ascending-order for everything.
-
-### Can I make changes to the backend or API?
-
-No, your final solution should not include any changes to the server code.
-
-### What should I do when I'm finished?
-
-Please use Git and make regular commits while working on this app. After you have finished your work upload your app to a Github repo, and then send us an email with the link to that repo along with information about which features you have included in your solution.
-
-### How is the exam graded?
-
-We are looking for idiomatic use of javascript, and the ability to solve the problems with code that is clean and easy to read. Even though it's very small in scope, please show us how you would use the language and conventions to structure things in a clear and maintainable way.
-
-Try to create a simple and elegant UI for this. You are free to use SASS or any other CSS pre-processor of your choice, but plain CSS is fine too.
-
-### This looks like it will take a while and I'm pretty busy
-
-You're right! With something open-ended like this you could easily spend a week polishing and getting it just right. We don't expect you to do this, and we'll do our best to make sure you're not disadvantaged by this.
-
-When we grade this exam we're not giving you a "score out of 100" for how many features you complete. We're trying to get some insight into your process, to see the way you work. So, by all means, spend more time if you want to. But you are also free to leave certain features out and give a written explanation of how you would approach it. The best approach is to spend your time on the features that you think is the best way to show us your strengths and experience.
+Install packages via npm
+```
+npm install or yarn
+```
+Start app
+```
+npm start / yarn start
+```
--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
-import Panel from './Panel';
+import InfiniteScroll from "react-infinite-scroller";
+import Panel from "./Panel";
 import "./App.css";
+
+const BASE_URL = "http://localhost:3000";
 
 class App extends Component {
   state = {
     idFilter: "",
     selectFilter: "",
-    products: []
+    products: [],
+    hasMore: true
   };
 
-  componentDidMount() {
-    fetch("http://localhost:3000/api/products?_limit=10", {
+  componentWillMount() {
+    fetch(`${BASE_URL}/api/products?_limit=15`, {
       method: "GET",
       headers: {
         Accept: "application/json"
@@ -19,7 +23,11 @@ class App extends Component {
       .then((response) => {
         if (response.ok) {
           response.json().then((json) => {
-            this.setState(() => ({ products: json }));
+            if (json.length > 0) {
+              this.setState(() => ({ products: json }));
+            } else {
+              this.setState({ hasMore: false });
+            }
           });
         }
       })
@@ -35,17 +43,45 @@ class App extends Component {
   filterBySizeOrPrize = (products, selectFilter, idFilter) => {
     if (selectFilter.length > 0) {
       return products.sort((a, b) => a[selectFilter] - b[selectFilter]);
-    } else if(idFilter.length > 0) {
-      return products.filter(product => product.id.includes(idFilter))
+    } else if (idFilter.length > 0) {
+      return products.filter((product) => product.id.includes(idFilter));
     } else {
       return products;
     }
   };
 
+  loadMoreProducts = (page) => {
+    fetch(`${BASE_URL}/api/products?_page=${page}&_limit=15`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then((response) => {
+        if (response.ok) {
+          const clonedProducts = [...this.state.products];
+          response.json().then((json) => {
+            if (json.length > 0) {
+              clonedProducts.push(...json);
+              this.setState(() => ({ products: clonedProducts }));
+            } else {
+              this.setState({ hasMore: false });
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        this.setState(() => ({ fetchError: err }));
+      });
+  };
+
   render() {
-    const { idFilter, products, selectFilter } = this.state;
-    const sortedProducts = this.filterBySizeOrPrize(products, selectFilter, idFilter);
-    // console.log({products})
+    const { idFilter, products, selectFilter, hasMore } = this.state;
+    const sortedProducts = this.filterBySizeOrPrize(
+      products,
+      selectFilter,
+      idFilter
+    );
 
     return (
       <React.Fragment>
@@ -67,16 +103,22 @@ class App extends Component {
             className="search_input"
             placeholder="Search for faces with id"
           />
-          <input
-            type="button"
-            value="&#x1F50D;"
-            className="search__btn"
-          />
+          <input type="button" value="&#x1F50D;" className="search__btn" />
         </nav>
         <main className="main__wrapper">
-          {sortedProducts.map((product) => (
-            <Panel key={product.id} product={product} />
-          ))}
+          <InfiniteScroll
+            pageStart={0}
+            initialLoad={false}
+            loadMore={this.loadMoreProducts}
+            threshold={1000}
+            hasMore={hasMore}
+            loader={<h3 className="hasMore">Loading ...</h3>}
+          >
+            {sortedProducts.map((product, index) => (
+              <Panel key={`${product.id}${index}`} product={product} />
+            ))}
+          </InfiniteScroll>
+          {!hasMore && <h3 className="hasMore">~ end of catalogue ~</h3>}
         </main>
       </React.Fragment>
     );
@@ -84,4 +126,3 @@ class App extends Component {
 }
 
 export default App;
-
